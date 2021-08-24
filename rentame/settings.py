c65fabad4c8219515c09c241b2f685df3c5270aa
Edgar Fernando Rodriguez <zeronoaz@gmail.com>
2021-08-23 22:39:01 -0500

    Arreglando secret key

--- rentame/settings.py
+++ rentame/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from secret_settings import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j=kibjyh2gi8y31y724k8#5auk^i6rv&!ionx+8#*1=*!gofrh'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
